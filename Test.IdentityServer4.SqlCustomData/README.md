7fa0651e0084c2428dc679b42b47164a389016bc
dedalusmax <dedalus123@gmail.com>
2018-05-04 11:26:09 +0200

    adding project with custom sql data

---
+++ Test.IdentityServer4.SqlCustomData/README.md
@@ -0,0 +1,18 @@
+﻿The code is based from the following blog post:
+
+https://mcguirev10.com/2018/01/02/identityserver4-without-entity-framework.html
+
+STEPS: 
+
+* new ASP.NET Core web app (empty), localhost:5000
+* install: IdentityServer4
+* powershell script in the root dir:
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/IdentityServer/IdentityServer4.Quickstart.UI/release/get.ps1'))
+
+* new database (McGuire), execute init.sql script
+* appsettings.json with AppDB connection string
+
+* Config.cs
+* Startup.cs
+* persistence folder with files
---
+++ Test.IdentityServer4.SqlCustomData/Startup.cs
@@ -0,0 +1,63 @@
+﻿using IdentityServer4;
+using IdentityServer4.Stores;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Test.IdentityServer4.SqlCustomData.Persistence;
+
+namespace Test.IdentityServer4.SqlCustomData
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddProfileService<UserProfileService>();
+
+            services.AddSingleton<IUserStore, UserStore>();
+            services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
+                    options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                try
+                {
+                    var configuration = app.ApplicationServices.GetService<TelemetryConfiguration>();
+                    configuration.DisableTelemetry = true;
+                }
+                catch { }
+            }
+
+            app.UseIdentityServer(); // includes a call to UseAuthentication
+
+            // That’s it - if you run the server and navigate the browser to:
+            // http://localhost:5000/.well-known/openid-configuration
+            // you should see the so-called discovery document. This will be used by your clients and APIs to download the necessary configuration data.
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
